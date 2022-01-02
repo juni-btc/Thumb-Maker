84c5e9ec997d258ff72266f290f86851e9567f1a
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-01-03 00:29:07 +0900

    feat: load Effect 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import './App.css';
+import styled from 'styled-components';
 import Header from './coponents/layout/Header';
 import Main from './coponents/layout/Main';
 // import Footer from './coponents/layout/Footer';
 import ThumbMaker from './coponents/ThumbMaker';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [loaded, setLoaded] = useState('');
+
+  React.useEffect(()=>{
+    setLoaded('loaded');
+  }, [])
+
   return (
-    <div className="App">
+    <Wrap className="App" className={loaded}>
       <div id="wrap">
         <Header/>
           <Main>
             <ThumbMaker/>
           </Main>
       </div>
-    </div>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  &.loaded{
+    animation: loadEffect 0.8s ease forwards;
+  }
+@keyframes loadEffect {
+    0%{ opacity: 0; transform: translateY(-40px); }
+    100%{ opacity: 100%; transform: translateY(0); }
+}
+`
 
 export default App;
